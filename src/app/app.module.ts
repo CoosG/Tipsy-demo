c672b44a10d24e99205e218b61da11a625ab2264
wessellourens <wessel7.lourens@gmail.com>
2019-09-09 20:58:17 +0200

    Adding firebase imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,20 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseService } from './providers/firebase.service';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAAN4_fzeWnM4PTNA4j-ZoNF_gJBNQk8tc',
+  authDomain: 'potch-map-1566554712889.firebaseapp.com',
+  databaseURL: 'https://potch-map-1566554712889.firebaseio.com',
+  projectId: 'potch-map-1566554712889',
+  storageBucket: 'potch-map-1566554712889.appspot.com',
+  messagingSenderId: '439304547193',
+  appId: '1:439304547193:web:d6d0c67bcd2f4c5fb39da8'
+};
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -21,10 +35,12 @@ import { environment } from '../environments/environment';
     IonicStorageModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production
-    })
+    }),
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   declarations: [AppComponent],
-  providers: [InAppBrowser, SplashScreen, StatusBar],
+  providers: [InAppBrowser, SplashScreen, StatusBar, FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
