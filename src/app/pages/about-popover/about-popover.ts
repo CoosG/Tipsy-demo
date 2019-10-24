be3b848eb41f3309d4d3f13aadd2d5e390d1560f
Cobus Greyling <coosgreyling@gmail.com>
2019-10-24 20:36:02 +0200

    updates

--- src/app/pages/about-popover/about-popover.ts
+++ src/app/pages/about-popover/about-popover.ts
@@ -1,27 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { PopoverController } from '@ionic/angular';
+import { PopoverController } from "@ionic/angular";
 
 @Component({
-  template: `
-    <ion-list>
-      <ion-item button (click)="close('https://ionicframework.com/getting-started')">
-        <ion-label>Learn Ionic</ion-label>
-      </ion-item>
-      <ion-item button (click)="close('https://ionicframework.com/docs/')">
-        <ion-label>Documentation</ion-label>
-      </ion-item>
-      <ion-item button (click)="close('https://showcase.ionicframework.com')">
-        <ion-label>Showcase</ion-label>
-      </ion-item>
-      <ion-item button (click)="close('https://github.com/ionic-team/ionic')">
-        <ion-label>GitHub Repo</ion-label>
-      </ion-item>
-      <ion-item button (click)="support()">
-        <ion-label>Support</ion-label>
-      </ion-item>
-    </ion-list>
-  `
+  template: ``
 })
 export class PopoverPage {
   constructor(public popoverCtrl: PopoverController) {}
@@ -32,7 +14,7 @@ export class PopoverPage {
   }
 
   close(url: string) {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
     this.popoverCtrl.dismiss();
   }
 }
--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,18 +1,18 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from "@angular/core";
 
-import { PopoverController } from '@ionic/angular';
+import { PopoverController } from "@ionic/angular";
 
-import { PopoverPage } from '../about-popover/about-popover';
+import { PopoverPage } from "../about-popover/about-popover";
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html',
-  styleUrls: ['./about.scss'],
+  selector: "page-about",
+  templateUrl: "about.html",
+  styleUrls: ["./about.scss"]
 })
 export class AboutPage {
-  conferenceDate = '2047-05-17';
+  conferenceDate = "2019-10-25";
 
-  constructor(public popoverCtrl: PopoverController) { }
+  constructor(public popoverCtrl: PopoverController) {}
 
   async presentPopover(event: Event) {
     const popover = await this.popoverCtrl.create({
