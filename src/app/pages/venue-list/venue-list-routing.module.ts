c54e437f77dbfa8909643af5719fa8f8ffd89c70
Cobus Greyling <coosgreyling@gmail.com>
2019-10-25 00:29:06 +0200

    updates

--- src/app/pages/venue-list/venue-list-routing.module.ts
+++ src/app/pages/venue-list/venue-list-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { VenueListPage } from './venue-list';
+import { VenueListPage } from "./venue-list";
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: VenueListPage
-  },
+  }
 ];
 
 @NgModule({
