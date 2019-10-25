a3f5ed3df0542a5ebff09e9f0bfe8af793d98c86
Cobus Greyling <coosgreyling@gmail.com>
2019-10-25 10:12:36 +0200

    up

--- src/app/pages/tabs-page/tabs-page.ts
+++ src/app/pages/tabs-page/tabs-page.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
-import { MediaFile, CaptureError, CaptureVideoOptions } from '@ionic-native/media-capture';
-import { File } from '@ionic-native/file/ngx';
-import { Storage } from '@ionic/storage';
-import { MediaCapture } from '@ionic-native/media-capture/ngx';
-import { UploadService } from '../../uploads/shared/upload.service';
-import { Upload } from '../../uploads/shared/upload';
-import * as _ from 'lodash';
+import { Component, OnInit } from "@angular/core";
+import { Plugins, CameraResultType, CameraSource } from "@capacitor/core";
+import {
+  MediaFile,
+  CaptureError,
+  CaptureVideoOptions
+} from "@ionic-native/media-capture";
+import { File } from "@ionic-native/file/ngx";
+import { Storage } from "@ionic/storage";
+import { MediaCapture } from "@ionic-native/media-capture/ngx";
+import { UploadService } from "../../uploads/shared/upload.service";
+import { Upload } from "../../uploads/shared/upload";
+import * as _ from "lodash";
 
-const MEDIA_FILES_KEY = 'mediaFiles';
+const MEDIA_FILES_KEY = "mediaFiles";
 
 @Component({
-  templateUrl: 'tabs-page.html'
+  templateUrl: "tabs-page.html"
 })
 export class TabsPage implements OnInit {
   image: any;
   mediaFiles = [];
   selectedFiles: FileList;
   currentUpload: Upload;
-
 
   constructor(
     private mediaCapture: MediaCapture,
@@ -28,7 +31,6 @@ export class TabsPage implements OnInit {
   ) {}
 
   async takePhoto() {
-
     const result = await Plugins.Camera.getPhoto({
       quality: 75,
       allowEditing: true,
@@ -36,7 +38,7 @@ export class TabsPage implements OnInit {
       resultType: CameraResultType.Uri // either Base64 or uri (Bace64 is bad for production and performance)
     });
 
-    this.image = result.webPath ; // this.domSanitizer.bypassSecurityTrustResourceUrl(result && result.dataUrl);
+    this.image = result.webPath; // this.domSanitizer.bypassSecurityTrustResourceUrl(result && result.dataUrl);
   }
 
   takeVideo() {
@@ -44,22 +46,27 @@ export class TabsPage implements OnInit {
       limit: 1,
       duration: 30
     };
-    this.mediaCapture.captureVideo(options).then((res: MediaFile[]) => {
-      const capturedFile = res[0];
-      const fileName = capturedFile.name;
-      const dir = capturedFile['localURL'].split('/');
-      dir.pop();
-      const fromDirectory = dir.join('/');
-      const toDirectory = this.file.dataDirectory;
+    this.mediaCapture.captureVideo(options).then(
+      (res: MediaFile[]) => {
+        const capturedFile = res[0];
+        const fileName = capturedFile.name;
+        const dir = capturedFile["localURL"].split("/");
+        dir.pop();
+        const fromDirectory = dir.join("/");
+        const toDirectory = this.file.dataDirectory;
+        this.uploadSingle();
 
-      this.file.copyFile(fromDirectory , fileName , toDirectory , fileName).then(() => {
-        this.storeMediaFiles([{name: fileName, size: capturedFile.size}]);
-      }, err => {
-        console.log('err: ', err);
-      });
-    },
-    (err: CaptureError) => console.error(err));
-    this.uploadSingle();
+        this.file.copyFile(fromDirectory, fileName, toDirectory, fileName).then(
+          () => {
+            this.storeMediaFiles([{ name: fileName, size: capturedFile.size }]);
+          },
+          err => {
+            console.log("err: ", err);
+          }
+        );
+      },
+      (err: CaptureError) => console.error(err)
+    );
   }
 
   storeMediaFiles(files) {
@@ -88,12 +95,11 @@ export class TabsPage implements OnInit {
   uploadMulti() {
     const files = this.selectedFiles;
     const filesIndex = _.range(files.length);
-    _.each(filesIndex, (idx) => {
+    _.each(filesIndex, idx => {
       this.currentUpload = new Upload(files[idx]);
-      this.upSvc.pushUpload(this.currentUpload); }
-    );
+      this.upSvc.pushUpload(this.currentUpload);
+    });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
